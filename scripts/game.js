0e6d9d933633320e0481c3bcaaa78d489687e4bf
JustinFourie1993 <>
2023-09-07 14:22:28 +0000

    Add test for showTurns function  and function

--- scripts/game.js
+++ scripts/game.js
@@ -2,7 +2,8 @@ let game = {
     score: 0,
     currentGame: [],
     playerMoves: [],
-    choices: ["button1", "button2", "button3", "button4"]
+    turnNumber: 0,
+    choices: ["button1", "button2", "button3", "button4"],
 }
 
 function newGame() {
@@ -16,7 +17,7 @@ function newGame() {
 function addTurn() {
     game.playerMoves = [];
     game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
-    // showTurns()
+    showTurns()
 }
 
 function showScore() {
@@ -28,6 +29,17 @@ function lightsOn(circ) {
     setTimeout(() => {
         document.getElementById(circ).classList.remove("light");
     },400);
+};
+
+function showTurns(){
+    game.turnNumber = 0;
+    let turns = setInterval(() => {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length){
+            clearInterval(turns);
+        }
+    }, 800);
 }
 
-module.exports = { game, newGame, showScore, addTurn, lightsOn };
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
